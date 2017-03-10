7a6fb4c22e3ae44ca6ebeb3686691cea26265386
desmondthorn <desmondroessingh@w134-87-162-054.wireless.uvic.ca>
2017-03-10 13:14:56 -0800

    Git commit by desmondr, with changes to the main page, main style, and the script

--- Website/Scripts.js
+++ Website/Scripts.js
@@ -45,3 +45,29 @@ function removeInfo() {
     document.getElementsByClassName("infoBar")[0].style.marginTop="";
     document.getElementsByClassName("infoBar")[0].style.backgroundColor="";
 }
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+
+
+
